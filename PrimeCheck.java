3f109df9582789b939d1e877edba79bf3e57a646
Soham Goswami <88580275+iamsoham0001@users.noreply.github.com>
2022-08-26 16:17:34 +0530

    Update PrimeCheck.java

--- PrimeCheck.java
+++ PrimeCheck.java
@@ -1,32 +1,3 @@
-
-//check prime or not normally
-import java.util.*;
-public class PrimeCheck {
-    public static void main(String arg[]) {
-        Scanner sc = new Scanner(System.in); 
-
-        int n = sc.nextInt();
-        boolean isPrime = true;
-
-        if(n == 2 ) {
-            System.out.println("Its a Prime");
-        } else {
-                for(int i=2; i<=Math.sqrt(n); i++) {    //same as for(int i=2; i<=n-1; i++) but more optimized
-                    if(n%i == 0) {
-                    isPrime = false ;
-                    }
-                }
-                if(isPrime == true) {
-                System.out.println("Prime");
-                } else {
-                System.out.println("Not Prime");
-                }
-             }       
-         }
-}
-
-
-//check prime or not using function
 import java.util.*;
 
 public class PrimeCheck {
